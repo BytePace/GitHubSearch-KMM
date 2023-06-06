1d874294d9be5c09c1c4b78f818c68174ef620fd
Yan Zhestovskiy <yan.zhestovskiy@bytepace.com>
2023-06-06 13:58:50 +0600

    Added gradle plugin

---
+++ buildSrc/src/main/kotlin/android-setup.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    compileSdk = (findProperty("android.compileSdk") as String).toInt()
+    namespace = "com.myapplication.common"
+
+    defaultConfig {
+        minSdk = (findProperty("android.minSdk") as String).toInt()
+        targetSdk = (findProperty("android.targetSdk") as String).toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    sourceSets["main"].apply {
+        manifest.srcFile("src/androidMain/AndroidManifest.xml")
+        res.srcDirs("src/androidMain/res")
+        resources.srcDirs("src/commonMain/resources")
+    }
+}
---
+++ buildSrc/src/main/kotlin/multiplatform-compose-setup.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.library")
+    kotlin("multiplatform")
+    kotlin("kapt")
+    id("org.jetbrains.compose")
+}
+
+kotlin {
+    jvm("desktop")
+    android()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                implementation(compose.components.resources)
+            }
+        }
+        val desktopMain by getting {
+            dependencies {
+                implementation(compose.desktop.common)
+            }
+        }
+        val androidMain by getting
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+    }
+}
